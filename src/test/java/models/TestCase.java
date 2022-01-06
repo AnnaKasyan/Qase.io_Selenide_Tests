2f4265aebbaf11e796cab73afdcef1838aa76d7f
Anna Kasyan <arelyeve2@gmail.com>
2022-01-06 13:25:41 +0300

    fixed annotations

--- src/test/java/models/TestCase.java
+++ src/test/java/models/TestCase.java
@@ -2,10 +2,14 @@ package models;
 
 
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
 @Builder
 public class TestCase {
 
@@ -17,17 +21,4 @@ public class TestCase {
     private String severity;
     private String priority;
     private String type;
-
-    public TestCase() {}
-
-    public TestCase(String title, String description, String pre_conditions, String post_conditions, String suite, String severity, String priority, String type) {
-        this.title = title;
-        this.description = description;
-        this.pre_conditions = pre_conditions;
-        this.post_conditions = post_conditions;
-        this.suite = suite;
-        this.severity = severity;
-        this.priority = priority;
-        this.type = type;
-    }
 }
